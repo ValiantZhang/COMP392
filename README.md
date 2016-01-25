0c52d5b26d1987a3ed928ba7d6da7f88bf3306f7
Jacky Zhang <jacky_02468@hotmail.com>
2016-01-25 22:10:20 +0000

    new stuff

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# Lesson02 - Part 2
 
+Lesson02 - Part 2 for COMP392 - Advanced Graphics @ Centennial College
--- typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-
-/// <reference path="threejs/three.d.ts" />
-/// <reference path="dat-gui/dat-gui.d.ts" />
-/// <reference path="../Scripts/objects/control.ts" />
-/// <reference path="stats/stats.d.ts" />
