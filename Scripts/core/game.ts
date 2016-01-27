0763ec2c87293198287983b704f0b7571335ee4d
ValiantZhang <jacky_02468@hotmail.com>
2016-01-27 02:59:53 +0000

    changed to object3d for type

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -71,7 +71,7 @@ function init() {
     console.log("Added Plane Primitive to scene...");
     
     
-    blobbyBoy = new gameObject();
+    blobbyBoy = new THREE.object3D();
      
     //Add a Sphere to the humanoid (head)
     sphere = new SphereGeometry(4, 10, 20);
