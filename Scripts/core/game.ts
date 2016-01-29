add6a08f87828c2b143abe16720e8815af3febd3
ValiantZhang <jacky_02468@hotmail.com>
2016-01-29 00:42:35 +0000

    reverted

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -174,7 +174,7 @@ function onResize(): void {
 
 
 function addControl(controlObject: Control): void {
-    gui.add(controlObject, 'rotationSpeedX', -0.5, 0.5);
+    gui.add(controlObject, 'rotationSpeed', -0.5, 0.5);
     //gui.add(controlObject, 'addCube');
     //gui.add(controlObject, 'removeCube');
     //gui.add(controlObject, 'outputObjects');
